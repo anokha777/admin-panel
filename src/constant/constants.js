bddb46bca5564498cfb396c66060b7e212660b33
Brajesh Anokha <anokha777@gmail.com>
2021-10-01 23:23:30 +0530

    certificate added

--- src/constant/constants.js
+++ src/constant/constants.js
@@ -1,6 +1,6 @@
 const appConstants = {
     // backendHost: 'http://localhost:9002', // local
-    backendHost: 'http://34.133.214.12:9002', // server
+    backendHost: 'https://34.133.214.12:8443', // server
   }
     
 module.exports = appConstants;
