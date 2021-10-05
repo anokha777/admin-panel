64672e0ab9ac26d565df111377392ee2656ab0e4
Brajesh Anokha <anokha777@gmail.com>
2021-10-06 01:59:48 +0530

    ssl configured

--- src/constant/constants.js
+++ src/constant/constants.js
@@ -1,6 +1,8 @@
 const appConstants = {
     // backendHost: 'http://localhost:9002', // local
-    backendHost: 'https://34.133.214.12:8443', // server
+    // backendHost: 'https://34.133.214.12:8443', // server
+    // backendHost: 'https://localhost:8443' // local docker-compose
+    backendHost: 'https://api.avyaja.org:8443' // server docker-compose
   }
     
 module.exports = appConstants;
